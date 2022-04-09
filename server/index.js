1ef61f4c429e614ba637e7c33ccbdf0a659769cc
priemum <50713962+priemum@users.noreply.github.com>
2022-04-09 10:25:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const config = require("./config/key");
 // const mongoose = require("mongoose");
 // mongoose
 //   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
+//   .then(() => console.log("DB connected"))           
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
